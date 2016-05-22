1d39f281aed195ad0f4e47c2213a0bbc49b55530
chisun-joung <csjoung@gmail.com>
2016-05-23 03:11:38 +0900

    start express

--- server.js
+++ server.js
@@ -12,26 +12,13 @@
 
 // console.log('Server running at http://localhost:3000/');
 
-var connect = require('connect');
-var app = connect();
+var connect = require('express');
+var app = express();
 
-var logger = function(req,res,next){
-    console.log(req.method, req.url);
-    next();
-};
-
-var helloWorld = function(req,res,next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-};
-
-var goodbyWorld = function(req,res,next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-};
-
-app.use(logger);
-app.use('/hello',helloWorld);
-app.use('/goodbye',goodbyWorld)
+app.use('/', function(req,res) {
+    res.send('Hello World');
+});
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
+
+module.exports = app;
