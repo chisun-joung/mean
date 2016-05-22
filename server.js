656e722a627fa4a3289a4b15cfb50d442f47291e
chisun-joung <csjoung@gmail.com>
2016-05-23 02:04:08 +0900

    goodbye

--- server.js
+++ server.js
@@ -20,14 +20,18 @@ var logger = function(req,res,next){
     next();
 };
 
-var hellloWorld = function(req,res,next) {
+var helloWorld = function(req,res,next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 };
 
+var goodbyWorld = function(req,res,next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
 
 app.use(logger);
-app.use(hellloWorld);
-
+app.use('/hello',helloWorld);
+app.use('/goodbye',goodbyWorld)
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
