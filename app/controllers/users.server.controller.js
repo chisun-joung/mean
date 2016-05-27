9cf731c2ace6f243a14f99a312988f0a9046b11c
chisun-joung <csjoung@gmail.com>
2016-05-27 15:58:59 +0900

    sign in

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,4 +1,49 @@
-var User = require('mongoose').model('User');
+var User = require('mongoose').model('User'),
+    passport = require('passport');
+
+var getErrorMessage = function(err) {
+    var message = '';
+    
+    if(err.code){
+        switch(err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong';
+        }
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) message = err.errors[errName].message;
+        }
+    }
+    
+    return message;
+};
+
+exports.renderSignin = function(req, res, next) {
+    if(!req.user) {
+        res.render('signin' , {
+            title: 'Sign-in Form',
+            messages: req.flash('error') || req.flash('info')
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.renderSingup = function(req, res, next) {
+    if(!req.user) {
+        res.render('signup', {
+            title: 'Sign-up Form' ,
+            messages: req.flash('error')
+        });
+    } else {
+        return res.redirect('/');
+    } 
+};
+
 
 exports.create = function (req,res,next) {
     var user = new User(req.body);
