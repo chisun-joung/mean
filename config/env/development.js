d829823599ba6ae53b78a31c07f8b41738f547a7
chisun-joung <csjoung@gmail.com>
2016-05-30 05:59:56 +0900

    twitter google oauth

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,21 @@
 module.exports = {
     db: 'mongodb://localhost/mean-book',
-    sessionSecret: 'developmentSessionSecret'
+    sessionSecret: 'developmentSessionSecret',
+    
     facebook: {
     clientID: '1041630435913150',
     clientSecret: '5573e76425a2379309f5758df2a8cb38',
     callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  },
+    twitter: {
+    clientID: 'Application Id',
+    clientSecret: 'Application Secret',
+    callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+  },
+  
+  google: {
+    clientID: 'Application Id',
+    clientSecret: 'Application Secret',
+    callbackURL: 'http://localhost:3000/oauth/google/callback'
   }
 };
