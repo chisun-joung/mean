01c0f20e9a48ef952eb7f93699f44b02eb301d13
chisun-joung <csjoung@gmail.com>
2016-05-29 21:24:35 +0900

    facebook auth

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
     db: 'mongodb://localhost/mean-book',
     sessionSecret: 'developmentSessionSecret'
+    facebook: {
+    clientID: '1041630435913150',
+    clientSecret: '5573e76425a2379309f5758df2a8cb38',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
 };
