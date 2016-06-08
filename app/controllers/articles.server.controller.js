b7639673fcb665e3e8b6bc073c01546dfa2c7bfe
chisun-joung <csjoung@gmail.com>
2016-06-08 17:15:46 +0900

    articles model

--- app/controllers/articles.server.controller.js
+++ app/controllers/articles.server.controller.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose'),
     Article = mongoose.model('Article');
+    
+    var getErrorMessage = function(err) {
+        if(err.errors) {
+            for( var errName in err.errors) {
+                if(err.errors[errName].message) return err.errors[errName].message;
+            }
+        } else {
+            return 'Unknown server error' ;
+        }
+    };
