d931d18d34b8d443181a842d91509a89e0334b84
chisun-joung <csjoung@gmail.com>
2016-06-08 14:53:04 +0900

    autthentication error  not fix

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,9 +1,5 @@
 exports.render = function (req,res) {
-    if(req.session.lastVisit) {
-        console.log(req.session.lastVisit);
-    }
-    
-    req.session.lastVisit = new Date();
+
     
     res.render('index', {
         title: 'MEAN STACK 연습',
