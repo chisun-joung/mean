36ab5cb8e359151880bceb904b69a7162f845f1a
chisun.joung <chisun.joung@lge.com>
2016-06-09 16:43:27 +0900

    using ngResource

---
+++ public/articles/controllers/articles.client.controller.js
@@ -0,0 +1,5 @@
+angular.module('articles').controller('ArticlesController', ['$scope', '$routeParams', '$location', 'Authentication', 'Articles',
+  function($scope, $routeParams, $location, Authentication, Articles) {
+    $scope.authentication = Authentication;
+  }
+]);
