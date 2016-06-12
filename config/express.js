1573274d284f20af257f07c2a698fc99f98e543e
chisun-joung <csjoung@gmail.com>
2016-06-12 18:40:57 +0900

    installing Socket.io

--- config/express.js
+++ config/express.js
@@ -7,6 +7,7 @@ var config = require('./config'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
     flash = require('connect-flash'),
     passport = require('passport');
 
@@ -25,10 +26,14 @@ module.exports = function () {
     }));
     app.use(bodyParser.json());
     app.use(methodOverride());
+    var mongoStore = new MongoStore({
+        db: db.connection.db
+    });
     app.use(session({
         saveUninitialized: true,
         resave: true,
-        secret: config.sessionSecret
+        secret: config.sessionSecret,
+        store: mongoStore
     }));
     app.set('views','./app/views');
     app.set('view engine', 'ejs');
@@ -42,5 +47,6 @@ module.exports = function () {
     require('../app/routes/articles.server.routes.js')(app);
 
     app.use(express.static('./public'));
+    require('./socketio')(server, io, mongoStore);
     return server;
 };
