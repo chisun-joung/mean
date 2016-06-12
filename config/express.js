4a9b34770290c5e22f883939b575ac16bb355495
chisun-joung <csjoung@gmail.com>
2016-06-12 09:30:17 +0900

    divorce 1

--- config/express.js
+++ config/express.js
@@ -1,4 +1,6 @@
 var config = require('./config'),
+    http = require('http'),
+    socketio = require('socket.io'),
     express = require('express'),
     morgan = require('morgan'),
     compress = require('compression'),
@@ -10,6 +12,9 @@ var config = require('./config'),
 
 module.exports = function () {
     var app = express();
+    var server = http.createServer(app);
+    var io = socketio.listen(server);
+
     if(process.env.NODE_ENV === 'development') {
         app.use(morgan('dev'));
     } else if (process.env.NODE_ENV === 'production') {
@@ -37,5 +42,5 @@ module.exports = function () {
     require('../app/routes/articles.server.routes.js')(app);
 
     app.use(express.static('./public'));
-    return app;
+    return server;
 };
